5ade9ed2f5e2ecf3ce4d0d6d8b4ee3dd831c3aa4
Christopher Welborn <cj@welbornprod.com>
2019-02-24 17:17:16 -0600

    More tests added, for parsing.
    ..more to come. There are a lot of weird cases.

--- test/data.py
+++ test/data.py
@@ -10,14 +10,76 @@ from ..lib.util.parser import (
 
 # Test data for MozaikMasterFile:
 mozmasterfile = {
-    # No room number, multi-cabs.
-    '3,2,42,BR,3&5&6,Frame': {
+    '1,2,42,BR,R1:1,Frame': {
+        'desc': 'Basic single part with room number.',
+        'no_split': [
+            MozaikMasterPart({
+                'count': 1,
+                'width': '2',
+                'length': '42',
+                'type': 'BR',
+                'no': 'R1:1',
+                'extra_data': 'Frame',
+            })
+        ],
+        'split': [
+            MozaikMasterPart({
+                'count': 1,
+                'width': '2',
+                'length': '42',
+                'type': 'BR',
+                'no': 'R1:1',
+                'extra_data': 'Frame',
+            }),
+        ],
+    },
+    '2,2,42,BR,R1:1 R2:2&3,Frame': {
+        'desc': 'Bad mozaik count, should be fixed automatically.',
         'no_split': [
             MozaikMasterPart({
                 'count': 3,
                 'width': '2',
                 'length': '42',
-                'no': 'R1:3&5&6',
+                'type': 'BR',
+                'no': 'R1:1 R2:2&3',
+                'extra_data': 'Frame',
+            })
+        ],
+        'split': [
+            MozaikMasterPart({
+                'count': 1,
+                'width': '2',
+                'length': '42',
+                'type': 'BR',
+                'no': 'R1:1',
+                'extra_data': 'Frame',
+            }),
+            MozaikMasterPart({
+                'count': 1,
+                'width': '2',
+                'length': '42',
+                'type': 'BR',
+                'no': 'R2:2',
+                'extra_data': 'Frame',
+            }),
+            MozaikMasterPart({
+                'count': 1,
+                'width': '2',
+                'length': '42',
+                'type': 'BR',
+                'no': 'R2:3',
+                'extra_data': 'Frame',
+            }),
+        ],
+    },
+    '3,2,42,BR,3&5&6,Frame': {
+        'desc': 'No room number, multi-cabs.',
+        'no_split': [
+            MozaikMasterPart({
+                'count': 3,
+                'width': '2',
+                'length': '42',
+                'no': '3&5&6',
                 'type': 'BR',
                 'extra_data': 'Frame',
             })
@@ -47,10 +109,10 @@ mozmasterfile = {
                 'type': 'BR',
                 'extra_data': 'Frame',
             }),
-        ]
+        ],
     },
-    # One room, multi-cabs.
     '8,1.5,42.5,TR,R2:9&10&11&13&15&16&17&18,': {
+        'desc': 'One room, multi-cabs.',
         'no_split': [
             MozaikMasterPart({
                 'count': 8,
@@ -126,6 +188,6 @@ mozmasterfile = {
                 'no': 'R2:18',
                 'extra_data': '',
             }),
-        ]
+        ],
     },
 }
--- test/test_tigertamer.py
+++ test/test_tigertamer.py
@@ -10,6 +10,8 @@
 import sys
 import unittest
 
+from colr import Colr as C
+
 from ..lib.util.parser import (
     MozaikMasterFile,
 )
@@ -17,26 +19,160 @@ from ..lib.util.parser import (
 from ..test import data
 
 
-class MozaikMasterPartTests(unittest.TestCase):
+class _NotSet(object):
+    def __bool__(self):
+        return False
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return '<Not Set>'
+
+
+NotSet = _NotSet()
+
+
+def part_compare_fmt(a, b, mark_keys=None):
+    """ Return a formatted string with two parts side by side. """
+    maxwidth = 80
+    halfwidth = maxwidth // 2
+    itemwidth = maxwidth // 4
+    mark_keys = set(mark_keys or [])
+    lines = [
+        '{typea:<{cols}}{typeb:<{cols}}'.format(
+            typea=C('{}:'.format(type(a).__name__).center(halfwidth), 'blue'),
+            typeb=C('{}:'.format(type(b).__name__).center(halfwidth), 'blue'),
+            cols=maxwidth // 2,
+        )
+    ]
+    header = set(a.header)
+    header.update(set(b.header))
+    fmt = '{key}={vala} {key}={valb}'
+    for key in sorted(header):
+        if (key not in a.header) or (key not in b.header):
+            # Auto-mark missing header keys.
+            mark_keys.add(key)
+
+        if key in mark_keys:
+            itemcolr = 'red'
+            keyfmt = '*{}'.format(key)
+        else:
+            itemcolr = 'cyan'
+            keyfmt = key
+        lines.append(fmt.format(
+            key=C(keyfmt, itemcolr).rjust(itemwidth),
+            vala=C(str(getattr(a, key, NotSet)), itemcolr).ljust(itemwidth),
+            valb=C(str(getattr(b, key, NotSet)), itemcolr).ljust(itemwidth),
+        ))
+    return '\n'.join(lines)
+
+
+def partlist_compare_fmt(lista, listb):
+    """ Show two parts lists side by side, highlighting any differences.
+        Returns a formatted str if the lists differ, otherwise returns
+        an empty str ('').
+    """
+    if lista == listb:
+        return ''
+    lena, lenb = len(lista), len(listb)
+    maxwidth = 80
+    halfwidth = maxwidth // 2
+    lines = []
+    for i in range(max(lena, lenb)):
+        try:
+            itema = lista[i]
+        except IndexError:
+            itema = NotSet
+        try:
+            itemb = listb[i]
+        except IndexError:
+            itemb = NotSet
+        symbol = '==' if itema == itemb else '!='
+        colora = 'red' if itema is NotSet else 'cyan'
+        colorb = 'red' if itemb is NotSet or itemb != itema else 'cyan'
+        lines.append(
+            str(C('\n').join(
+                '   {}'.format(C(itema, colora).ljust(halfwidth)),
+                '{} {}'.format(symbol, C(itemb, colorb)),
+            ))
+        )
+
+    return '\n'.join(lines)
+
+
+def part_diff(a, b):
+    """ Compare two parts, and return a formatted str with the difference.
+        If there is no difference, then empty str is returned.
+    """
+    if a.header != b.header:
+        return part_compare_fmt(a, b)
+    mark_keys = set()
+    for key in a.header:
+        vala = getattr(a, key, NotSet)
+        valb = getattr(b, key, NotSet)
+        if vala != valb:
+            mark_keys.add(key)
+    if not mark_keys:
+        # No difference.
+        return ''
+    return part_compare_fmt(a, b, mark_keys=mark_keys)
+
+
+class MozaikMasterFileTests(unittest.TestCase):
 
     def setUp(self):
         """ Set up test data for MozaikMasterPartTests. """
         self.testdata = data.mozmasterfile
 
-    def assertPartEqual(self, a, b, **kwargs):
-        """ Like assertEqual, but has better messages for MozaikParts. """
+    def assertPartEqual(self, a, b, msg=None):
+        """ Like assertEqual, but with better messages for MozaikParts. """
         try:
-            self.assertEqual(a, b, **kwargs)
+            self.assertEqual(a, b, msg=msg)
         except AssertionError:
-            lines = [kwargs.get('msg', 'Parts are not equal')]
-            for key in a.header:
-                lines.append('{}={:<30} {}={}'.format(
-                    key,
-                    getattr(a, key, None),
-                    key,
-                    getattr(b, key, None),
-                ))
+            lines = [msg or 'Parts are not equal']
+            lines.append(part_diff(a, b))
             raise AssertionError('\n'.join(lines))
+
+    def assertPartListEqual(self, a, b, msg=None, desc=None):
+        """ Like assertListEqual, but with better repr's for parts. """
+        if a == b:
+            return
+        lena, lenb = len(a), len(b)
+        diffindex = -1
+        for i in range(min(lena, lenb)):
+            itema = a[i]
+            itemb = b[i]
+            if itema != itemb:
+                diffindex = i
+                parta = itema
+                partb = itemb
+                diffmsg = 'Parts are not equal.'
+                break
+        else:
+            # One is a subset of the other.
+            diffindex = min(lena, lenb) - 1
+            parta = a[diffindex]
+            partb = b[diffindex]
+            if lena > lenb:
+                diffmsg = 'First list is larger.'
+            elif lenb > lenb:
+                diffmsg = 'Second list is larger.'
+
+        lines = [
+            str(C(msg or 'Lists are not equal.', 'red')),
+        ]
+        if desc:
+            lines.append('   Description: {}'.format(C(desc, 'cyan')))
+
+        lines.append('\n{}'.format(partlist_compare_fmt(a, b)))
+        lines.append(
+            '{} First differing index: {}'.format(diffmsg, diffindex)
+        )
+        lines.append('\nFirst differing part:\n{}'.format(
+            part_diff(parta, partb)
+        ))
+        raise AssertionError('\n'.join(lines))
 
     def test_parse_line(self):
         """ parse_line should create valid MozaikParts. """
@@ -44,18 +180,20 @@ class MozaikMasterPartTests(unittest.TestCase):
         for line, cases in self.testdata.items():
             # Test without split parts.
             mfile = MozaikMasterFile.from_line(line, split_parts=False)
-            self.assertListEqual(
+            self.assertPartListEqual(
                 mfile.parts,
                 cases['no_split'],
                 msg='from_line(split_parts=False) failed to parse correctly.',
+                desc=cases['desc'],
             )
 
             # Test with split parts.
             mfile = MozaikMasterFile.from_line(line, split_parts=True)
-            self.assertListEqual(
+            self.assertPartListEqual(
                 mfile.parts,
                 cases['split'],
                 msg='from_line(split_parts=True) failed to parse correctly.',
+                desc=cases['desc'],
             )
 
 
